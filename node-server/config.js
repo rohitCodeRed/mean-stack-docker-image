3c7143e710b198b506d90c87aa8272773e7990a5
rohitCodeRed <rohit06051993@gmail.com>
2022-12-10 19:51:58 +0530

    Script tested with some chnages

--- node-server/config.js
+++ node-server/config.js
@@ -3,7 +3,7 @@ const config = {};
 config.privateKey = "secretKey";
 config.dbPort="27017";
 config.dbName="Chart";
-config.dbHost="MONGO_DB"
+config.dbHost="db"
 config.mongoDB_url = `mongodb://${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = config;
